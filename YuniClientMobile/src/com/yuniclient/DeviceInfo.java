e7a2109f0ee1fe29a96eed4ff6192943290f256b
Tassadar <vbocek@gmail.com>
2011-07-19 14:17:52 +0200

    [70] Add support for ATmega8u2

--- YuniClientMobile/src/com/yuniclient/DeviceInfo.java
+++ YuniClientMobile/src/com/yuniclient/DeviceInfo.java
@@ -8,10 +8,11 @@ public class DeviceInfo
         if     (id.contentEquals("m16")) { name = "ATmega16";  mem_size = 16128; page_size = 128; }
         else if(id.contentEquals("m32")) { name = "ATmega32";  mem_size = 32256; page_size = 128; }
         else if(id.contentEquals("m48")) { name = "ATmega48";  mem_size = 3840;  page_size = 64; patch_pos = 3838; }
-        else if(id.contentEquals("m88")) { name = "ATmega88";  mem_size = 7936;  page_size = 64; }
+        else if(id.contentEquals("m88")) { name = "ATmega88";  mem_size = 7936;  page_size = 64;  }
         else if(id.contentEquals("m168")){ name = "ATmega168"; mem_size = 16128; page_size = 128; }
         else if(id.contentEquals("m162")){ name = "ATmega162"; mem_size = 16128; page_size = 128; }
         else if(id.contentEquals("m328")){ name = "ATmega328"; mem_size = 32256; page_size = 128; }
+        else if(id.contentEquals("m8u2")){ name = "ATmega8u2"; mem_size = 7680;  page_size = 128; }
         /* FIXME: only 16-bit addresses are available */
         else if(id.contentEquals("m128")){ name = "ATmega128"; mem_size = 65536; page_size = 256; }
     }
