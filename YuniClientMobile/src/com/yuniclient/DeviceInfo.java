cc0f2ba0e3383bcae77dc094f8ab5fe8f44a6368
Tassadar <vbocek@gmail.com>
2011-08-02 18:58:59 +0200

    [m73] Add support for ATmega1284P

--- YuniClientMobile/src/com/yuniclient/DeviceInfo.java
+++ YuniClientMobile/src/com/yuniclient/DeviceInfo.java
@@ -5,16 +5,18 @@ public class DeviceInfo
     DeviceInfo(String id)
     {
         mem_size = 0; page_size = 0; patch_pos = 0; name = null;
-        if     (id.contentEquals("m16")) { name = "ATmega16";  mem_size = 16128; page_size = 128; }
-        else if(id.contentEquals("m32")) { name = "ATmega32";  mem_size = 32256; page_size = 128; }
-        else if(id.contentEquals("m48")) { name = "ATmega48";  mem_size = 3840;  page_size = 64; patch_pos = 3838; }
-        else if(id.contentEquals("m88")) { name = "ATmega88";  mem_size = 7936;  page_size = 64;  }
-        else if(id.contentEquals("m168")){ name = "ATmega168"; mem_size = 16128; page_size = 128; }
-        else if(id.contentEquals("m162")){ name = "ATmega162"; mem_size = 16128; page_size = 128; }
-        else if(id.contentEquals("m328")){ name = "ATmega328"; mem_size = 32256; page_size = 128; }
-        else if(id.contentEquals("m8u2")){ name = "ATmega8u2"; mem_size = 7680;  page_size = 128; }
+        if     (id.contentEquals("m16")) { name = "ATmega16";    mem_size = 16128; page_size = 128; }
+        else if(id.contentEquals("m32")) { name = "ATmega32";    mem_size = 32256; page_size = 128; }
+        else if(id.contentEquals("m48")) { name = "ATmega48";    mem_size = 3840;  page_size = 64; patch_pos = 3838; }
+        else if(id.contentEquals("m88")) { name = "ATmega88";    mem_size = 7936;  page_size = 64;  }
+        else if(id.contentEquals("m168")){ name = "ATmega168";   mem_size = 16128; page_size = 128; }
+        else if(id.contentEquals("m162")){ name = "ATmega162";   mem_size = 16128; page_size = 128; }
+        else if(id.contentEquals("m328")){ name = "ATmega328P";  mem_size = 32256; page_size = 128; }
+        else if(id.contentEquals("m8u2")){ name = "ATmega8u2";   mem_size = 7680;  page_size = 128; }
         /* FIXME: only 16-bit addresses are available */
-        else if(id.contentEquals("m128")){ name = "ATmega128"; mem_size = 65536; page_size = 256; }
+        else if(id.contentEquals("m128")){ name = "ATmega128";   mem_size = 65536; page_size = 256; }
+        /* FIXME: only 16-bit addresses are available and too long chip name */
+        else if(id.contentEquals("p128")){ name = "ATmega1284P"; mem_size = 65536; page_size = 256; }
     }
     
     public boolean isSet() { return (mem_size != 0); }
